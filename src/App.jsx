4f119edff2ba8ad60056f5f69ff2b77c58531de1
RM78451 <106501994+RM78451@users.noreply.github.com>
2024-06-14 23:22:30 -0600

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
@@ -10,7 +8,7 @@ function App() {
     <>
       <header>
         <div className="logo">
-          <img src="./img/Logo.png" alt="" />
+          <img src="/img/Logo.png" alt="Logo INSISA" />
           <h1>INSISA</h1>
         </div>
 
@@ -37,15 +35,17 @@ function App() {
           <h1>INSISA</h1>
           <h3>Innovación en seguridad integral</h3>
         </div>
-        <img src="./img/Logo.png" alt="" />
+        <img src="/img/Logo.png" alt="Logo INSISA" />
       </main>
 
       <section>
         <div className="nosotros">
           <h1>¿Quienes somos?</h1>
-          <p>INSISA es una empresa nicaraguense especializada en seguridad integral, con más de una década de experiencia en el sector. Autorizada con la Licencia de Operación A-181, INSISA se destaca por ofrecer soluciones innovadoras y tecnológicas en el ámbito de la seguridad privada. Su misión es proporcionar a los clientes servicios de seguridad integrales y eficientes, demostrando su compromiso con la excelencia en cada aspecto de su trabajo.</p>
+          <p>
+            INSISA es una empresa nicaragüense especializada en seguridad integral, con más de una década de experiencia en el sector. Autorizada con la Licencia de Operación A-181, INSISA se destaca por ofrecer soluciones innovadoras y tecnológicas en el ámbito de la seguridad privada. Su misión es proporcionar a los clientes servicios de seguridad integrales y eficientes, demostrando su compromiso con la excelencia en cada aspecto de su trabajo.
+          </p>
         </div>
-        <img src="./img/nosotros.jpg" alt="" />
+        <img src="/img/nosotros.jpg" alt="Nosotros" />
       </section>
 
       <div className="servicios">
@@ -53,15 +53,21 @@ function App() {
         <div className="terjetas-servicios">
           <div className="tarjeta">
             <h3>Sistemas de videovigilancia y monitoreo</h3>
-            <p>INSISA podría instalar sistemas de videovigilancia de última tecnología para monitorear y grabar áreas específicas, lo que permite a los clientes tener un control visual en tiempo real de sus instalaciones.</p>
+            <p>
+              INSISA podría instalar sistemas de videovigilancia de última tecnología para monitorear y grabar áreas específicas, lo que permite a los clientes tener un control visual en tiempo real de sus instalaciones.
+            </p>
           </div>
           <div className="tarjeta">
             <h3>Control de accesos y presencia</h3>
-            <p>INSISA podría implementar sistemas de control de accesos para regular quién puede entrar a determinadas áreas restringidas dentro de una empresa o residencia.</p>
+            <p>
+              INSISA podría implementar sistemas de control de accesos para regular quién puede entrar a determinadas áreas restringidas dentro de una empresa o residencia.
+            </p>
           </div>
           <div className="tarjeta">
             <h3>Consultoría en seguridad</h3>
-            <p>INSISA podría ofrecer servicios de consultoría en seguridad para evaluar los riesgos existentes en un entorno, proponer medidas preventivas y correctivas, y diseñar planes de seguridad a medida.</p>
+            <p>
+              INSISA podría ofrecer servicios de consultoría en seguridad para evaluar los riesgos existentes en un entorno, proponer medidas preventivas y correctivas, y diseñar planes de seguridad a medida.
+            </p>
           </div>
         </div>
       </div>
@@ -69,7 +75,9 @@ function App() {
       <div className="contactos">
         <div className="informacion-contactos">
           <h1>Contáctanos</h1>
-          <p>¡Protege lo que más valoras con INSISA, tu aliado en seguridad integral! Contáctanos hoy y descubre cómo podemos resguardar tus activos de manera efectiva y personalizada. ¡Tu tranquilidad es nuestra prioridad!</p>
+          <p>
+            ¡Protege lo que más valoras con INSISA, tu aliado en seguridad integral! Contáctanos hoy y descubre cómo podemos resguardar tus activos de manera efectiva y personalizada. ¡Tu tranquilidad es nuestra prioridad!
+          </p>
         </div>
 
         <div className="iconos-contactos">
